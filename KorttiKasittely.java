22ff023d7a1c2e722f1fab9dde7f7c90ef5f569f
Habatsuki <36292528+Habatsuki@users.noreply.github.com>
2018-03-12 17:46:32 +0200

    Update KorttiKasittely.java

--- KorttiKasittely.java
+++ KorttiKasittely.java
@@ -1,15 +1,15 @@
 package oliot_projekti;
 
+import java.awt.Image;
 import java.util.ArrayList;
+import javax.swing.ImageIcon;
 
 //Korttien käsittely (Funktiot)
 public class KorttiKasittely {
     
     //Luo korttioliot suoraan korttipakkaan
     public static void LuoKorttipakka(ArrayList<Kortti> kortitPakka) {
-        int iKorttiIndex = 0;
-        
-        //Luo kortti oliot pakka arrayhin
+        int iKorttiIndex = 0;        
         //For kortin maat (1-4)
         for (int iIndexMaat = 1; iIndexMaat <= 4; iIndexMaat++) {
             //For kortin arvot (1-13)
@@ -23,182 +23,181 @@ public class KorttiKasittely {
         kortitPakka.add(iKorttiIndex, new Kortti(iKorttiIndex, 0, 0, true));
     }
     
-    //Kortin nosto funktio
-    public static void NostaKortti(ArrayList<Kortti> kortitPakka, ArrayList<Kortti> kortitKasi) {
-        int iNostettavaKortti;
+    //Nosta kortti haluttuun positioon käsikortteihin
+    public static void NostaKortti(ArrayList<Kortti> kortitPakka, ArrayList<Kortti> kortitKasi, int iKorttiIndex) {    
+        int iSatunnainenKortti;
         //Valitaan pakasta satunnainen indeksi
-        iNostettavaKortti = (int) (Math.random() * kortitPakka.size());
-        //Näytä nostettu kortti
-        System.out.println("Nostit kortin " + kortitPakka.get(iNostettavaKortti));
+        iSatunnainenKortti = (int) (Math.random() * kortitPakka.size());
         //Siirrä nostettu kortti käsikortteihin
-        kortitKasi.add(kortitPakka.get(iNostettavaKortti));
-        
-        //Kutsu funktiota joka piirtää kortin peliruutuun
-        //esim. Texture class jossa on metodi joka ottaa vastaan kortin indexin (1-5) ja Kortti objektin
-        //Texture.PiirraKortti(kortitKasi.size(), kortitPakka.get(iNostettavaKortti));
-        
+        kortitKasi.add(iKorttiIndex, kortitPakka.get(iSatunnainenKortti));
+        //Aseta käsikortin kuva
+        KorttiKasittely.AsetaKasiKorttiKuva(iKorttiIndex, kortitPakka.get(iSatunnainenKortti));
         //Poista kortti pakasta
-        kortitPakka.remove(iNostettavaKortti);
-    }
-    
-    //Uusi kortinnostofunktio joka vaatii kortin position (0-4) ja piirtää nostetun kortin siihen
-    public static void NostaKortti(ArrayList<Kortti> kortitPakka, ArrayList<Kortti> kortitKasi, int iKorttiIndex) {
-        
-        int iNostettavaKortti;
-        javax.swing.JLabel targetLabel;
-        
-        //Valitaan pakasta satunnainen indeksi
-        iNostettavaKortti = (int) (Math.random() * kortitPakka.size());
-        //Siirrä nostettu kortti käsikortteihin
-        kortitKasi.add(iKorttiIndex, kortitPakka.get(iNostettavaKortti));
-        
-        //Mikä kortin tiedostonimi on?
-        String sKuvannimi = kortitPakka.get(iNostettavaKortti).getKorttiTiedostoNimi();
-        
-        //Mihin labeliin kuva tulee
-        switch (iKorttiIndex) {
-            case 0:
-                targetLabel = Gui_Main.jLab1_Kortti_1;
-                break;
-            case 1:
-                targetLabel = Gui_Main.jLab2_Kortti_2;
-                break;
-            case 2:
-                targetLabel = Gui_Main.jLab3_Kortti_3;
-                break;
-            case 3:
-                targetLabel = Gui_Main.jLab4_Kortti_4;
-                break;
-            default:
-                targetLabel = Gui_Main.jLab5_Kortti_5;
-                break;
-        }
-        
-        //targetLabelissä on se haluttu label
-        Image im=new ImageIcon(kortitPakka.get(iNostettavaKortti).getClass().getResource("/Kuvat/"+sKuvannimi+".png")).getImage();
-        ImageIcon kortinKuva = new ImageIcon(im);
-        
-        targetLabel.setIcon(kortinKuva);
-        
-        //Poista kortti pakasta
-        kortitPakka.remove(iNostettavaKortti);
-    }
-        
-        //targetLabelissä on se haluttu label
-        Image im=new ImageIcon(kortitPakka.get(iNostettavaKortti).getClass().getResource("/Kuvat/"+sKuvannimi+".png")).getImage();
-        ImageIcon kortinKuva = new ImageIcon(im);
-        
-        targetLabel.setIcon(kortinKuva);
-        
-        //Poista kortti pakasta
-        kortitPakka.remove(iNostettavaKortti);
+        kortitPakka.remove(iSatunnainenKortti);
     }
     
     //Nostetaan kortteja kunnes käsikorteissa on viisi korttia
     public static void NostaKasiTayteenKortteja(ArrayList<Kortti> kortitPakka, ArrayList<Kortti> kortitKasi) {
-        int iPositio = 0;
+        int iKorttiIndex = 0;
         while (kortitKasi.size() < 5) {
-            //Ilmoita funktiolle mihin positioon nostettu kortti laitetaan
             //Tämä sen vuoksi että jäljelle jäävät kortit eivät siirry
-            KorttiKasittely.NostaKortti(kortitPakka, kortitKasi, iPositio);
-            iPositio++;
+            KorttiKasittely.NostaKortti(kortitPakka, kortitKasi, iKorttiIndex);
+            //Aseta kortin kuva
+            KorttiKasittely.AsetaKasiKorttiKuva(iKorttiIndex, kortitKasi.get(iKorttiIndex));
+            iKorttiIndex++;
         }
     }
     
     //Korttien sekoitus takaisin pakkaan
-    public static void SiirraKortitToiseenKasaan(ArrayList<Kortti> kortitTarget, ArrayList<Kortti> kortitSource) {
-        //Sekoitetaan kortit takaisin pakkaan
-        while (kortitSource.size() > 0) {
-            //Siirrä nostettu kortti käsikortteihin
-            kortitTarget.add(kortitSource.get(0));
-            //Poista kortti pakasta
-            kortitSource.remove(0);
+    public static void SiirraKaikkiKortitPakkaan(ArrayList<Kortti> kortitPakka, ArrayList<Kortti> kortitKasi, ArrayList<Kortti> kortitMuu) {
+        //Siirretään käsikortit pakkaan
+        while (kortitKasi.size() > 0) {
+            //Siirrä käsikortti pakkaan
+            kortitPakka.add(kortitKasi.get(0));
+            //Poista kortti käsikorteista
+            kortitKasi.remove(0);
+        }
+        //Siirretään muut kortit pakkaan
+        while (kortitMuu.size() > 0) {
+            //Siirrä muu kortti pakkaan
+            kortitPakka.add(kortitMuu.get(0));
+            //Poista kortti muu korteista
+            kortitMuu.remove(0);
         }
     }
+
+//KOODISSA ONGELMIA JOTEN HOIDAN SEN MUUALLA MANUAALISESTI
+//    public static void AsetaKasiKorttiTyhja(int iKorttiIndex) {
+//        javax.swing.JLabel targetLabel;
+//            
+//        switch (iKorttiIndex) {
+//            case 4:
+//                targetLabel = Gui_Main.jLabel5;
+//                break;
+//            case 3:
+//                targetLabel = Gui_Main.jLabel4;
+//                break;
+//            case 2:
+//                targetLabel = Gui_Main.jLabel3;
+//                break;
+//            case 1:
+//                targetLabel = Gui_Main.jLabel2;
+//                break;
+//            default:
+//                targetLabel = Gui_Main.jLabel1;
+//                break;
+//        }
+//        //Aseta kortin kuva tyhjäksi
+//        Image im = new ImageIcon("/Kuvat/cardback.png").getImage();
+//        
+//        ImageIcon kortinKuva = new ImageIcon(im);
+//        
+//        targetLabel.setIcon(kortinKuva);
+//    }
     
-    //Korttien sekoitus takaisin pakkaan
-    public static void SiirraKaikkiKortitToiseenKasaan(ArrayList<Kortti> kortitTarget, ArrayList<Kortti> kortitSource) {
-        while (kortitSource.size() > 0) {
-            //Siirrä nostettu kortti käsikortteihin
-            kortitTarget.add(kortitSource.get(0));
-            //Poista kortti pakasta
-            kortitSource.remove(0);
+    public static void AsetaKasiKorttiKuva(int iKorttiIndex, Kortti tempKortti) {
+        javax.swing.JLabel targetLabel;
+        
+        switch (iKorttiIndex) {
+            case 4:
+                targetLabel = Gui_Main.jLabel5;
+                break;
+            case 3:
+                targetLabel = Gui_Main.jLabel4;
+                break;
+            case 2:
+                targetLabel = Gui_Main.jLabel3;
+                break;
+            case 1:
+                targetLabel = Gui_Main.jLabel2;
+                break;
+            default:
+                targetLabel = Gui_Main.jLabel1;
+                break;
         }
+        
+        //Mikä kortin tiedostonimi on?
+        String sKuvannimi = tempKortti.getKorttiTiedostoNimi();
+        
+        //targetLabelissä on se haluttu label
+        Image im=new ImageIcon(tempKortti.getClass().getResource("/Kuvat/"+sKuvannimi+".png")).getImage();
+        ImageIcon kortinKuva = new ImageIcon(im);
+        
+        targetLabel.setIcon(kortinKuva);
+    }
+    
+    //Tarkistetaan onko halutun indexin kortti lukittu
+    public static boolean OnkoKorttiLukittu(int iKorttiIndex) {
+        javax.swing.JCheckBox targetCheckbox;
+        
+        switch (iKorttiIndex) {
+            case 4:
+                targetCheckbox = Gui_Main.jCheckBox5;
+                break;
+            case 3:
+                targetCheckbox = Gui_Main.jCheckBox4;
+                break;
+            case 2:
+                targetCheckbox = Gui_Main.jCheckBox3;
+                break;
+            case 1:
+                targetCheckbox = Gui_Main.jCheckBox2;
+                break;
+            default:
+                targetCheckbox = Gui_Main.jCheckBox1;
+                break;
+        }
+        return targetCheckbox.isSelected();
     }
     
     //Korttien sekoitus takaisin pakkaan
     public static void SiirraVaihdettavatKortitMuuKasaan(ArrayList<Kortti> kortitMuu, ArrayList<Kortti> kortitKasi) {
         //Looppaa checkboxeja
+        //Checkboxeja loopataan käänteisessä järjestyksessä koska arraylist.remove(x) siirtää x jälkeiset kortit yhden vasemmalle (pienemmäksi)
         for (int iKorttiIndex = 4; iKorttiIndex >= 0; iKorttiIndex--) {
-            javax.swing.JLabel targetLabel;
-            javax.swing.JCheckBox targetCheckbox;
-            
-            //Remove kortit käänteisessä järjestyksessä
-            switch (iKorttiIndex) {
-                case 4:
-                    targetCheckbox = Gui_Main.jC_B_5;
-                    targetLabel = Gui_Main.jLab5_Kortti_5;
-                    break;
-                case 3:
-                    targetCheckbox = Gui_Main.jC_B_4;
-                    targetLabel = Gui_Main.jLab4_Kortti_4;
-                    break;
-                case 2:
-                    targetCheckbox = Gui_Main.jC_B_3;
-                    targetLabel = Gui_Main.jLab3_Kortti_3;
-                    break;
-                case 1:
-                    targetCheckbox = Gui_Main.jC_B_2;
-                    targetLabel = Gui_Main.jLab2_Kortti_2;
-                    break;
-                default:
-                    targetCheckbox = Gui_Main.jC_B_1;
-                    targetLabel = Gui_Main.jLab1_Kortti_1;
-                    break;
-            }
-            //Jos kortti ei ole lukittu niin siirrä se muu kasaan
-            if (targetCheckbox.isSelected()) {
-                //Jos kortti on lukittu
+            if (KorttiKasittely.OnkoKorttiLukittu(iKorttiIndex)) {
+                //Jos kortti on lukittu niin ei tehdä mitään
             } else {
-                //Siirrä vaihdettava kortti muu kortteihin
-                kortitMuu.add(kortitKasi.get(iKorttiIndex-1));
+                //Lisää vaihdettava käsikortti muu kortteihin
+                kortitMuu.add(kortitKasi.get(iKorttiIndex));
                 //Poista kortti kädestä
-                kortitKasi.remove(iKorttiIndex-1);
-                //Aseta kortin kuva tyhjäksi
-                ImageIcon kortinKuva = new ImageIcon("/Kuvat/cardback.png");
-                targetLabel.setIcon(kortinKuva);
+                kortitKasi.remove(iKorttiIndex);
+                //Piirrä käsikortti tyhjäksi
+                    //KOODISSA ONGELMIA JOTEN HOIDAN SEN MUUALLA MANUAALISESTI
+                    //KorttiKasittely.AsetaKasiKorttiTyhja(iKorttiIndex);
             }
         }
     }
     
     //Korttien sekoitus takaisin pakkaan
+    //Uudet kortit nostetaan index paikkoihin niihin paikkoihin josta äsken vaihdettiin kortteja
     public static void NostaUudetKortitVaihdettujenTilalle(ArrayList<Kortti> kortitPakka, ArrayList<Kortti> kortitKasi) {
         javax.swing.JCheckBox targetCheckbox;
         
         //Looppaa checkboxeja
-        for (int iKorttiPositio = 0; iKorttiPositio <= 4; iKorttiPositio++) {
-            switch (iKorttiPositio) {
+        for (int iKorttiIndex = 0; iKorttiIndex <= 4; iKorttiIndex++) {
+            switch (iKorttiIndex) {
                 case 0:
-                    targetCheckbox = Gui_Main.jC_B_1;
+                    targetCheckbox = Gui_Main.jCheckBox1;
                     break;
                 case 1:
-                    targetCheckbox = Gui_Main.jC_B_2;
+                    targetCheckbox = Gui_Main.jCheckBox2;
                     break;
                 case 2:
-                    targetCheckbox = Gui_Main.jC_B_3;
+                    targetCheckbox = Gui_Main.jCheckBox3;
                     break;
                 case 3:
-                    targetCheckbox = Gui_Main.jC_B_4;
+                    targetCheckbox = Gui_Main.jCheckBox4;
                     break;
                 default:
-                    targetCheckbox = Gui_Main.jC_B_5;
+                    targetCheckbox = Gui_Main.jCheckBox5;
                     break;
             }
             if (targetCheckbox.isSelected()) {
                 //Jos lukittu niin ei tarvitse tehdä mitään
             } else {
-                //Nosta kortti haluttuun positioon
-                KorttiKasittely.NostaKortti(kortitPakka, kortitKasi, iKorttiPositio);
+                //Nosta kortti tähän index positioon
+                KorttiKasittely.NostaKortti(kortitPakka, kortitKasi, iKorttiIndex);
             }
         }
     }
